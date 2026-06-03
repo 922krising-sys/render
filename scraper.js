8e2ab84f9733d4be72163df4a5772db08ebcf22a
922krising-sys <922krise@gmail.com>
2026-06-02 20:50:09 -0700

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -14,8 +14,14 @@ let browser;
 async function getBrowser() {
   if (!browser) {
     browser = await puppeteer.launch({
-      headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu'],
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--single-process',
+      ],
     });
   }
   return browser;
